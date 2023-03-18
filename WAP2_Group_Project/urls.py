872c790114ffd45a9a0c659654b0b56c157ed23b
Luke McColl <2578891m@student.gla.ac.uk>
2023-03-18 13:23:26 +0000

    Created Showtalk Folder

--- WAP2_Group_Project/urls.py
+++ WAP2_Group_Project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from showtalk import views
 
 urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('showtalk/', include('showtalk.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ showtalk/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ShowtalkConfig(AppConfig):
+    name = 'showtalk'
