9644105a91b5052185040fd6032cfb83430a0185
Luke McColl <2578891m@student.gla.ac.uk>
2023-03-18 14:05:39 +0000

    +Static, Media folders.

--- WAP2_Group_Project/urls.py
+++ WAP2_Group_Project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from showtalk import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('showtalk/', include('showtalk.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
