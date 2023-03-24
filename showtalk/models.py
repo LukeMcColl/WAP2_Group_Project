23f00bfd24260ce4b6ffe7de4446d02fc7b0f7ad
eilidh <56912943+SMC242@users.noreply.github.com>
2023-03-24 05:07:02 +0000

    Added field names for user

--- showtalk/models.py
+++ showtalk/models.py
@@ -3,45 +3,44 @@ from django.db import models
 
 class user(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField('Account',max_length=20,default='',unique=True)
-    password = models.CharField('Password',max_length=20, default='')
-    name = models.CharField("Name",max_length=20,default='')
-    img = models.ImageField(upload_to='img',default='img/images.png')
-    email = models.EmailField('Email',max_length=20,default='')
-    bio = models.CharField('',max_length=200,default='')
-    favourite_shows = models.CharField('', max_length=200, default='')
+    username = models.CharField("Account name", max_length=20, default="", unique=True)
+    password = models.CharField("Password", max_length=20, default="")
+    name = models.CharField("Name", max_length=20, default="")
+    img = models.ImageField("Avatar", upload_to="img", default="img/images.png")
+    email = models.EmailField("Email", max_length=20, default="")
+    bio = models.CharField("Biography", max_length=200, default="")
+    favourite_shows = models.CharField("Favourite shows", max_length=200, default="")
 
-    create_time = models.DateTimeField('create time', auto_now_add=True)
-    updated_time = models.DateTimeField('updated time', auto_now=True)
+    create_time = models.DateTimeField("create time", auto_now_add=True)
+    updated_time = models.DateTimeField("updated time", auto_now=True)
 
     class Meta:
-        db_table = 'user'
-        verbose_name_plural = 'user'
-
-
+        db_table = "user"
+        verbose_name_plural = "user"
 
 
 class tv(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.CharField(max_length=20,default='')
+    user_id = models.CharField(max_length=20, default="")
 
-    title = models.CharField(max_length=20,default='')
-    docs = models.CharField('docs', max_length=200, default='')
-    tv = models.FileField(upload_to='tv',default='tv/demo.mp4')
+    title = models.CharField(max_length=20, default="")
+    docs = models.CharField("docs", max_length=200, default="")
+    tv = models.FileField(upload_to="tv", default="tv/demo.mp4")
 
-    create_time = models.DateTimeField('create time', auto_now_add=True)
-    updated_time = models.DateTimeField('updated time', auto_now=True)
+    create_time = models.DateTimeField("create time", auto_now_add=True)
+    updated_time = models.DateTimeField("updated time", auto_now=True)
 
     class Meta:
-        db_table = 'tv'
+        db_table = "tv"
+
 
 class pl(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.CharField(max_length=20,default='')
-    tv_id =  models.CharField(max_length=20,default='')
+    user_id = models.CharField(max_length=20, default="")
+    tv_id = models.CharField(max_length=20, default="")
     doc = models.TextField()
-    create_time = models.DateTimeField('create time', auto_now_add=True)
-    updated_time = models.DateTimeField('updated time', auto_now=True)
+    create_time = models.DateTimeField("create time", auto_now_add=True)
+    updated_time = models.DateTimeField("updated time", auto_now=True)
 
     class Meta:
-        db_table = 'pl'
+        db_table = "pl"
