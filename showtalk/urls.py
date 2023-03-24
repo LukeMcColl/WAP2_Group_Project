35ea6da7435b53cbd7d5154cdafa94c411472149
eilidh <56912943+SMC242@users.noreply.github.com>
2023-03-24 03:46:53 +0000

    Merged homepage and index

--- showtalk/urls.py
+++ showtalk/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
 
-app_name = 'showtalk'
+app_name = "showtalk"
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('login',views.login,name='login'),
-    path('register',views.register,name='register'),
-    path('profile',views.profile,name='profile'),
-    path('logout',views.logout,name='logout'),
-    path('tv',views.tvv,name='tv'),
-    path('pl',views.pinglun,name='pl'),
+    path("", views.homepage, name="homepage"),
+    path("<int:page_id>", views.homepage, name="homepage"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("profile", views.profile, name="profile"),
+    path("logout", views.logout, name="logout"),
+    path("tv", views.tvv, name="tv"),
+    path("pl", views.pinglun, name="pl"),
 ]
